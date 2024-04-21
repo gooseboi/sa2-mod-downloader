76d5fee47ffc522fdf23bb9c7e55c0033397c5a9
Guzman Zugnoni <gooseiman@protonmail.com>
2024-04-21 06:46:41 -0300

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "Build a cargo project which uses SQLx";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, crane, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        craneLib = crane.lib.${system};
+        src = craneLib.cleanCargoSource ./.;
+
+        # Common arguments can be set here to avoid repeating them later
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+
+          nativeBuildInputs = [
+            pkgs.pkg-config
+            pkgs.cmake
+          ];
+        };
+
+        # Build *just* the cargo dependencies, so we can reuse
+        # all of that work (e.g. via cachix) when running in CI
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        # Build the actual crate itself, reusing the dependency
+        # artifacts from above.
+        bin = craneLib.buildPackage (commonArgs // {
+          inherit cargoArtifacts;
+        });
+      in
+      {
+        packages = {
+          default = bin;
+          inherit bin;
+        };
+
+        devShells.default = pkgs.mkShell {
+          # instead of passing `buildInputs` / `nativeBuildInputs`,
+          # we refer to an existing derivation here
+          inputsFrom = [ bin ];
+        };
+      });
+}
